89d4d22692fcb39955aa1b9a4f1c019c9f229081
Adam Svozilik <adam.svozilik@email.cz>
2020-11-23 14:09:20 +0100

    repair

--- packages/enhanced/lib/src/widgets/video_player.dart
+++ packages/enhanced/lib/src/widgets/video_player.dart
@@ -1,9 +1,7 @@
 import 'dart:io' show Platform;
 
-import 'package:chewie/chewie.dart' as lib;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:video_player/video_player.dart' as lib;
 
 /// A video player.
 class VideoPlayer extends StatefulWidget {
@@ -54,81 +52,7 @@ class VideoPlayer extends StatefulWidget {
   State<VideoPlayer> createState() =>
       (!kIsWeb && Platform.environment.containsKey('FLUTTER_TEST'))
           ? _VideoPlayerPlaceholder()
-          : _VideoPlayerState();
-}
-
-class _VideoPlayerState extends State<VideoPlayer> {
-  lib.ChewieController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = _Controller(this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) => lib.Chewie(controller: _controller);
-
-  void _onAspectRatioUpdated() => setState(() {});
-}
-
-class _Controller extends lib.ChewieController {
-  final _VideoPlayerState vps;
-
-  double _aspectRatio;
-
-  _Controller(this.vps)
-      : super(
-          autoInitialize: true,
-          autoPlay: vps.widget.autoplay == true,
-          looping: vps.widget.loop == true,
-          placeholder: vps.widget.poster != null
-              ? Center(child: vps.widget.poster)
-              : null,
-          showControls: vps.widget.controls == true,
-          videoPlayerController:
-              lib.VideoPlayerController.network(vps.widget.url),
-        ) {
-    if (vps.widget.autoResize) {
-      _setupAspectRatioListener();
-    }
-  }
-
-  @override
-  double get aspectRatio => _aspectRatio ?? vps.widget.aspectRatio;
-
-  @override
-  void dispose() {
-    super.dispose();
-    videoPlayerController.dispose();
-  }
-
-  void _setupAspectRatioListener() {
-    VoidCallback listener;
-
-    listener = () {
-      if (_aspectRatio == null) {
-        final vpv = videoPlayerController.value;
-        debugPrint('[_Controller]: vpv=$vpv');
-
-        if (!vpv.initialized) return;
-        _aspectRatio = vpv.aspectRatio;
-
-        // workaround because we cannot call `vps.setState()` directly
-        vps._onAspectRatioUpdated();
-      }
-
-      videoPlayerController.removeListener(listener);
-    };
-
-    videoPlayerController.addListener(listener);
-  }
+          : Container();
 }
 
 class _VideoPlayerPlaceholder extends State<VideoPlayer> {
